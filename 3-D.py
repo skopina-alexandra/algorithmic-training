b4d8e308667dc765130e69255e5106dab99b39bf
Alexandra Skopina <skopina.aa@outlook.com>
2024-03-26 20:22:13 +1000

    3-D

--- 3-D.py
+++ 3-D.py
@@ -2,14 +2,15 @@ n, k = map(int, input().split())
 numbers = list(map(int, input().split()))
 
 def hasRepeats(n, k, numbers):
-    current_window = set(numbers[1 : k + 1])
-    count = len(numbers)
-    for i in range(count - 1):
-        if numbers[i] in current_window:
-            return True
-        current_window.discard(numbers[i + 1])
-        if i + k < count:
-            current_window.add(numbers[i + k])
+    last_indices = {}
+    for i in range(n):
+        if last_indices.get(numbers[i]) == None:
+            last_indices.update({numbers[i]: i})
+        else:
+            if i - last_indices.get(numbers[i]) <= k:
+                return True
+            else:
+                last_indices.update({numbers[i]: i})
     return False
 
 result = hasRepeats(n, k, numbers)
